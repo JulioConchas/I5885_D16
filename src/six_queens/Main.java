030b587dc0712cee247db1a4b304fab054f28b8a
jconchas <jconchas@ibm.com>
2020-11-01 12:48:48 -0600

    6 REINAS: creación de proyecto

---
+++ src/six_queens/Main.java
@@ -0,0 +1,14 @@
+package six_queens;
+
+public class Main {
+
+/**
+ * Author: Julio Conchas
+ * Email : conchasjimenez@gmail.com
+ * */
+    public static void main(String[] args) {
+       System.out.println("Hola 6 reinas");
+
+    }
+
+}
