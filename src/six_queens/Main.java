08b783379f7b48dcc672107222d38c84f580e9e4
jconchas <jconchas@ibm.com>
2020-11-02 20:43:20 -0600

    6 REINAS, agregación de botones y titulo

--- src/six_queens/Main.java
+++ src/six_queens/Main.java
@@ -3,6 +3,7 @@ package six_queens;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -14,39 +15,63 @@ import javax.swing.border.EmptyBorder;
 
 public class Main extends JFrame{
     private JPanel contentPanel;
+    private JButton play_btn,pause_btn;
+    private JLabel name_label,title_label;
+    
+    private int _X = 75,_Y = 75;
     
     public Main() {
+        
+        contentPanel = new JPanel();
+        play_btn = new JButton("Play");
+        pause_btn = new JButton("Pause");
+        name_label = new JLabel("Julio Conchas");
+        title_label = new JLabel("6 Reinas, Sem Algoridmia (I5885)");
+        
+        setBounds(100,100,450,300);
+        play_btn.setBounds(650,250,100,30);
+        pause_btn.setBounds(550,250,100,30);
+        name_label.setBounds(400,10,300,30);
+        title_label.setBounds(350,25,300,30);
+        
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100,100,450,300);
-        contentPanel = new JPanel();
         contentPanel.setBorder(new EmptyBorder(5,5,5,5));
+        
+        contentPanel.add(play_btn);
+        contentPanel.add(pause_btn);
+        contentPanel.add(name_label);
+        contentPanel.add(title_label);
+        
         setContentPane(contentPanel);
         contentPanel.setLayout(null);
-        setBounds(0,0,800,600);
-    }
-    public void paint(Graphics g) {
-        int x=50,y=70;
-        super.paint(g);
-        g.setColor(Color.blue);
+        setBounds(0,0,800,600); 
         
+    }
+    private void _six_queens(Graphics _g) {
+        int x=75,y=75;
+        _g.setColor(Color.blue);
         for (int i = 0;i <= 5;i++) {
             for(int j = 0; j <=5;j++) {
                 if(j == 0) {
-                    g.setColor(Color.red);
-                    g.fillRect(x,y,50,70);
-                    x += 50;
+                    _g.setColor(Color.red);
+                    _g.fillRect(x,y,_X,_Y);
+                    x += _X;
                 } 
                 else {
-                    g.setColor(Color.blue);
-                    g.drawRect(x,y,50,70);
-                    x += 50;
+                    _g.setColor(Color.blue);
+                    _g.drawRect(x,y,_X,_Y);
+                    x += _X;
                 }
             }
-            x = 50;
-            y += 70;
+            x = _X;
+            y += _Y;
         }
     }
-    
+    public void paint(Graphics g) {
+        super.paint(g);
+        _six_queens(g); 
+    }
     public static void main(String[] args) {
       EventQueue.invokeLater(new Runnable() {
           public void run() {
